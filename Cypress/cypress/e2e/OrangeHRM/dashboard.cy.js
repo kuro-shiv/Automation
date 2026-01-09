696247b936e48ea1a69ec2d004b5d976e1ee892d
kuro-shiv <dubeysk2003@gmail.com>
2026-01-09 08:58:31 +0530

    support understanding

--- Cypress/cypress/e2e/OrangeHRM/dashboard.cy.js
+++ Cypress/cypress/e2e/OrangeHRM/dashboard.cy.js
@@ -1,10 +1,6 @@
 describe('Dashboard Validation',() => {
     beforeEach (() => {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.get('input[name="username"]').type('Admin')
-        cy.get('input[name="password"]').type('admin123')
-        cy.get('button[type="submit"]').click()
-        cy.url().should('include', '/dashboard')
+        cy.login('Admin','admin123')
     })
     it ('Validate Dashboard Elements', () => {
         cy.contains('Time at Work').should('be.visible')
